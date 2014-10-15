6c5a099fe74502315cd93dfb0061f974076e2b4e
Brent Woodruff <brent@fprimex.com>
2014-10-15 14:36:55 -0400

    use zdesk 2.0.3, add fetching and listing

--- zdkb/main.py
+++ zdkb/main.py
@@ -7,54 +7,123 @@ from __future__ import print_function
 import os
 import sys
 import textwrap
-import base64
+import simplejson as json
 
 import zdeskcfg
 from zdesk import Zendesk
 
 @zdeskcfg.configure(
     verbose=('verbose output', 'flag', 'v'),
+    listing=('print listing of a forum. use "all" to list everything',
+                      'option', 'l', None, None, '(FORUM_ID|all)'),
+    fetch=('download the Zendesk KB to KB_DIR', 'flag', 'f'),
     kb_dir=('directory containing the kb to upload',
                       'option', 'k', None, None, 'KB_DIR'),
     )
 def zdkb(verbose=False,
+        listing=None,
+        fetch=False,
         kb_dir=os.path.join(os.path.expanduser('~'), 'zdkb')):
     "Manage Zendesk knowledgbases"
 
     cfg = zdkb.getconfig()
 
-    if cfg['zdesk_url'] and cfg['zdesk_email'] and cfg['zdesk_password']:
-        if verbose:
-            print('Configuring Zendesk with:\n'
-                  '  url: {}\n'
-                  '  email: {}\n'
-                  '  token: {}\n'
-                  '  password: (hidden)\n'.format( cfg['zdesk_url'],
-                                                   cfg['zdesk_email'],
-                                                   repr(cfg['zdesk_token']) ))
-        zd = Zendesk(**cfg)
-    else:
-        msg = textwrap.dedent("""\
-            Error: Need Zendesk config to continue.
-
-            Config file (~/.zdeskcfg) should be something like:
-            [zdesk]
-            url = https://example.zendesk.com
-            email = you@example.com
-            password = dneib393fwEF3ifbsEXAMPLEdhb93dw343
-            token = 1
-
-            [kb]
-            kb_dir = /path/to/kb_dir
-            """)
-        print(msg)
-        return 1
-
     # Log the cfg
     if verbose:
         print('Running with zdkb config:\n'
               ' verbose: {}\n'
-              ' kb_dir: {}\n'.format(verbose, kb_dir))
+              ' listing: {}\n'
+              ' kb_dir: {}\n'.format(verbose, listing, kb_dir))
+
+    #if state['categories'] or state['topics'] or state['forums'] or state['list_zdf']:
+    if listing or fetch:
+        if cfg['zdesk_url'] and cfg['zdesk_email'] and cfg['zdesk_password']:
+            if verbose:
+                print('Configuring Zendesk with:\n'
+                      '  url: {}\n'
+                      '  email: {}\n'
+                      '  token: {}\n'
+                      '  password: (hidden)\n'.format( cfg['zdesk_url'],
+                                                       cfg['zdesk_email'],
+                                                       repr(cfg['zdesk_token']) ))
+            zd = Zendesk(**cfg)
+        else:
+            msg = textwrap.dedent("""\
+
+                Config file (e.g. ~/.zdf2pdf.cfg) should be something like:
+                [zdf2pdf]
+                url = https://example.zendesk.com
+                mail = you@example.com
+                password = dneib393fwEF3ifbsEXAMPLEdhb93dw343
+                is_token = 1
+                """)
+            print(msg)
+            msg = textwrap.dedent("""\
+                Error: Need Zendesk config for requested operation.
+
+                Config file (~/.zdeskcfg) should be something like:
+                [zdesk]
+                url = https://example.zendesk.com
+                email = you@example.com
+                password = dneib393fwEF3ifbsEXAMPLEdhb93dw343
+                token = 1
+
+                [zdkb]
+                kb_dir = /path/to/kb_dir
+                """)
+            print(msg)
+            return 1
+
+    # If any listing was requested we will do that and exit, regardless of
+    # any other supplied options.
+    if listing == 'all':
+        # List available zendesk forums with their IDs and titles and exit.
+        # Listing is formatted like for following:
+        # 12345 Category 1 name
+        #     09876 Forum 1 name
+        #     54321 Forum 2 name
+        # 67890 Category 2 name
+        if verbose:
+            print('Listing all forums')
+
+        response = zd.forums_list(get_all_pages=True)
+        forums = response['forums']
+        cat_ids = set(f['category_id'] for f in forums)
+
+        categories = zd.categories_list(get_all_pages=True)
+
+        for cat_id in cat_ids:
+            if cat_id:
+                try:
+                    cat_name = next(c['name'] for c in categories
+                                                if c['id'] == cat_id)
+                except StopIteration:
+                    cat_name = 'None'
+            else:
+                cat_id = 'None'
+                cat_name = 'None'
+            print('{} ({})'.format(cat_name, cat_id))
+
+            for forum in forums:
+                if repr(forum['category_id']) == cat_id:
+                    print('    {} ({})'.format(forum['name'], forum['id']))
+        return 0
+
+    elif listing:
+        if verbose:
+            print('Listing all entries in forum {}'.format(listing))
+
+        # List a zendesk forum's entries with their titles and IDs and exit
+        try:
+            forum_id = int(listing)
+        except ValueError:
+            print('Error: Could not convert to integer: {}'.format(listing))
+            return 1
+
+        entries = zd.forum_topics(id=listing, get_all_pages=True)
+        for entry in entries['topics']:
+            print('{} ({})'.format(entry['title'], entry['id']))
+        return 0
 
     # Save the current directory so we can go back once done
     start_dir = os.getcwd()
@@ -63,128 +132,66 @@ def zdkb(verbose=False,
     kb_dir = os.path.abspath(kb_dir)
 
     # Check for and create working directory
-    if not os.path.isdir(work_dir):
+    if not os.path.isdir(kb_dir):
         print('kb_dir does not exist: {}'.format(kb_dir))
-        return None
+        return 1
 
-    return None
+    if fetch:
+        # Change to working directory to begin file output
+        os.chdir(kb_dir)
 
-    # Change to working directory to begin file output
-    #os.chdir(kb_dir)
+        response = zd.forums_list(get_all_pages=True)
+        forums = response['forums']
 
-#    if tickets:
-#        # tickets given, query for those
-#        q = ' '.join(['ticket_id:' + s for s in map(str,tickets)])
-#    else:
-#        # List of tickets not given. Get all of the attachments for all of this
-#        # user's open tickets.
-#        q = 'status<solved assignee:{}'.format(agent)
-#
-#    if verbose:
-#        print('Retrieving tickets page 1')
-#
-#    response = zd.search(query=q)
-#
-#    if response['count'] == 0:
-#        # No tickets from which to get attachments
-#        print("No tickets provided for attachment retrieval.")
-#        return {}
-#
-#    results = response['results']
-#    page = 1
-#    while response['next_page'] != None:
-#        page += 1
-#
-#        if verbose:
-#            print('Retrieving tickets page {}'.format(page))
-#
-#        response = zd.search(query=q, page=page)
-#        results.extend(response['results'])
-#
-#    # Fix up some headers to use for downloading the attachments.
-#    # We're going to borrow the zdesk object's httplib client.
-#    headers = {}
-#    if zd.zdesk_email is not None and zd.zdesk_password is not None:
-#        headers["Authorization"] = "Basic {}".format(
-#            base64.b64encode(zd.zdesk_email.encode('ascii') + b':' +
-#                             zd.zdesk_password.encode('ascii')))
-#
-#    # Get the attachments from the given zendesk tickets
-#    for ticket in results:
-#        if ticket['result_type'] != 'ticket':
-#            # This is not actually a ticket. Weird. Skip it.
-#            continue
-#
-#        if verbose:
-#            print('Ticket {}'.format(ticket['id']))
-#
-#        ticket_dir = os.path.join(work_dir, str(ticket['id']))
-#        ticket_com_dir = os.path.join(ticket_dir, 'comments')
-#        comment_num = 0
-#
-#        if verbose:
-#            print('Retrieving audits page {}'.format(page))
-#
-#        response = zd.ticket_audits(ticket_id=ticket['id'])
-#        audits = response['audits']
-#        page = 1
-#        while response['next_page'] != None:
-#            page += 1
-#
-#            if verbose:
-#                print('Retrieving audits page {}'.format(page))
-#
-#            response = zd.ticket_audits(ticket_id=ticket['id'], page=page)
-#            audits.extend(response['audits'])
-#
-#        for audit in audits:
-#            for event in audit['events']:
-#                if event['type'] != 'Comment':
-#                    # This event isn't a comment. Skip it.
-#                    continue
-#
-#                comment_num += 1
-#                comment_dir = os.path.join(ticket_com_dir, str(comment_num))
-#
-#                if verbose and event['attachments']:
-#                    print('Comment {}'.format(comment_num))
-#
-#                for attachment in event['attachments']:
-#                    name = attachment['file_name']
-#                    if os.path.isfile(os.path.join(comment_dir, name)):
-#                        if verbose:
-#                            print('Attachment {} already present'.format(name))
-#                        continue
-#
-#                    # Get this attachment
-#                    if verbose:
-#                        print('Attachment {}'.format(name))
-#
-#                    # Check for and create the download directory
-#                    if not os.path.isdir(comment_dir):
-#                        os.makedirs(comment_dir)
-#
-#                    os.chdir(comment_dir)
-#                    response, content = zd.client.request(attachment['content_url'], headers=headers)
-#                    if response['status'] != '200':
-#                        print('Error downloading {}'.format(attachment['content_url']))
-#                        continue
-#
-#                    with open(name, 'wb') as f:
-#                        f.write(content)
-#
-#                    # Check for and create the grabs entry to return
-#                    if ticket_dir not in grabs:
-#                        grabs[ticket_dir] = []
-#
-#                    grabs[ticket_dir].append(
-#                        os.path.join('comments', str(comment_num), name) )
-#
-#                    # Let's try to extract this if it's compressed
-#                    zdsplode(name, verbose=verbose)
+        response = zd.categories_list(get_all_pages=True)
+        categories = response['categories']
 
-#    os.chdir(start_dir)
-#    return grabs
+        response = zd.topics_list(get_all_pages=True)
+        topics = response['topics']
+
+        with open('categories', 'w') as cat_file:
+            json.dump(categories, cat_file)
+
+        for forum in forums:
+            forum_name = forum['name'].replace('/', '-')
+
+            if not os.path.isdir(forum_name):
+                os.mkdir(forum_name)
+
+            os.chdir(forum_name)
+
+            with open(forum_name + '.json', 'w') as forum_json:
+                json.dump(forum, forum_json)
+
+            os.chdir(kb_dir)
+
+        for topic in topics:
+            try:
+                forum_name = next(f['name'] for f in forums
+                                            if f['id'] == topic['forum_id'])
+            except StopIteration:
+                forum_name = 'None'
+
+            forum_name = forum_name.replace('/', '-')
+
+            if not os.path.isdir(forum_name):
+                os.mkdir(forum_name)
+
+            os.chdir(forum_name)
+
+            topic_filename = topic['title'].replace('/', '-')
+
+            with open(topic_filename + '.html', 'w') as topic_html:
+                topic_html.write(topic['body'])
+
+            with open(topic_filename + '.json', 'w') as topic_json:
+                del topic['body']
+                json.dump(topic, topic_json)
+
+            os.chdir(kb_dir)
+
+        return 0
+    return 0
 
 
 def main(argv=None):
